dc792c04240ce125f5610651d0c496289aadcb9c
rloegoijecuador <puyol.black96@gmail.com>
2023-11-26 19:27:37 -0500

    agre one

--- src/components/pokedexpage/PokeCard.jsx
+++ src/components/pokedexpage/PokeCard.jsx
@@ -18,10 +18,10 @@ const handleNavigate =() => {
 }
   return (
     <article className="pokekard" onClick={handleNavigate}>
-      <heade className="pokekard_header">
+      <header className="pokekard_header">
         <img className="pokekard_image" 
         src={infoPoke?.sprites.other["official-artwork"].front_default} alt="" />
-      </heade>
+      </header>
       <section className="pokekard_body">
         <h3 className="pokekard_name">{infoPoke?.name}</h3>
         <ul className="pokekard_types">
@@ -39,7 +39,7 @@ const handleNavigate =() => {
             infoPoke?.stats.map((infoStat) =>(
               <li className="pokekard_stat" key={infoStat.stat.url}>   
                 <h4 className="pokekard_stat_name">{infoStat.stat.name}</h4>
-                <span className="pokekard_stat_name_value">{infoStat.base_stat}</span>
+                <span className="pokekard_stat_value">{infoStat.base_stat}</span>
                 </li> 
             ))
           }
